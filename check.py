8437226a0b15690744c7deb0cb29fee4a5e41d42
razeen <rubberaser0307@gmail.com>
2025-03-29 17:09:18 +1100

    fix and check

---
+++ check.py
@@ -0,0 +1,55 @@
+# python check.py --device "cuda:6"
+import argparse
+import torch
+import datetime
+import json
+import yaml
+import os
+from model.process import Process
+
+
+# Load Arguments
+parser = argparse.ArgumentParser(description="model and data")
+
+parser.add_argument("--data", type=str, default="physio", help="Name of the dataset to use")
+parser.add_argument('--device', default='cuda:0', help='Device for Attack')
+
+# parser.add_argument("--unconditional", action="store_true")
+parser.add_argument("--modelfolder", type=str, default="")
+parser.add_argument("--nsample", type=int, default=100)
+
+# parser.add_argument("--seed", type=int, default=1)
+# parser.add_argument("--testmissingratio", type=float, default=0.1)
+# parser.add_argument("--nfold", type=int, default=0, help="for 5fold test (valid value:[0-4])")
+
+args = parser.parse_args()
+print(args)
+
+# Load data config
+path = "config/" + args.data + ".yaml"
+with open(path, "r") as f:
+    config = yaml.safe_load(f)
+
+# config["model"]["is_unconditional"] = args.unconditional
+# config["model"]["test_missing_ratio"] = args.testmissingratio
+
+print(json.dumps(config, indent=4))
+
+from dataset.physio import get_dataloader
+train_loader_1, valid_loader_1, test_loader_1 = get_dataloader(
+    seed=config["data"]["seed"],
+    nfold=config["data"]["nfold"],
+    batch_size=config["data"]["batch_size"],
+    missing_ratio=config["data"]["test_missing_ratio"],
+    missing_pattern=config["data"]["missing_pattern"],
+)
+data_info = config["data"]["missing_pattern"] + "_" + str(config["data"]["test_missing_ratio"])
+
+from dataset.physio_old import get_dataloader
+train_loader_2, valid_loader_2, test_loader_2 = get_dataloader(
+    seed=config["data"]["seed"],
+    nfold=config["data"]["nfold"],
+    batch_size=config["data"]["batch_size"],
+    missing_ratio=config["data"]["test_missing_ratio"],
+    missing_pattern=config["data"]["missing_pattern"],
+)

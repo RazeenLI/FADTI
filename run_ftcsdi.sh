743bf74478f6cd35685f8cf0171180f89e75fa78
razeen <rubberaser0307@gmail.com>
2025-04-14 14:06:38 +1000

    add fun.sh

---
+++ run_ftcsdi.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+mkdir -p logs
+# 示例参数数组，可根据需要修改
+methods=("fbm" "frsst" "fsst")
+# ("csdi_ori" "csdi" "ftcsdi" "saits")
+datas=("ett")
+nfolds=(0 1 2 3 4)
+missrates=(0.1 0.5)
+misspatterns=("point" "time")
+# nsample 和 device 可以直接定义
+nsample=100
+device="cuda:6"
+
+# 外层循环遍历所有参数组合
+for method in "${methods[@]}"; do
+    for data in "${datas[@]}"; do
+         for nfold in "${nfolds[@]}"; do
+            for missrate in "${missrates[@]}"; do
+                for misspattern in "${misspatterns[@]}"; do
+                    # 构造输出文件名，可以根据需求修改命名规则
+                    output_file="logs/${model}_${data}_${nfold}.out"
+                    echo "Running: python run.py --model \"$model\" --data \"$data\" --nsample $nsample --device \"$device\" --nfold $nfold --misspattern \"$misspattern\" --missrate $missrate --method \"$method\" > $output_file"
+                    python run.py --model "ftcsdi" --data "$data" --nsample $nsample --device "$device" --nfold $nfold --misspattern "$misspattern" --missrate $missrate --method "$method" > "$output_file"
+                done
+            done
+         done
+    done
+done

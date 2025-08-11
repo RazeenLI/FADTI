da439a42d0183bd559a05f9f272736cdeb3cdaaa
Runze ICML <razeen.runze.li@gmail.com>
2025-08-11 05:37:49 +0000

    add utils

---
+++ utils/loaders.py
@@ -0,0 +1,267 @@
+from torch.optim import AdamW, Adam
+
+
+def get_dataloader(args, config):
+    if args.data == "physio":
+        from dataset.physio import get_dataloader
+        train_loader, valid_loader, test_loader, scaler = get_dataloader(
+            seed=config["data"]["seed"],
+            nfold=args.nfold,
+            batch_size=config["data"]["batch_size"],
+            missing_ratio=args.missrate, # config["data"]["test_missing_ratio"],
+            missing_pattern=args.misspattern, # config["data"]["missing_pattern"],
+            num_steps=config["data"]["num_steps"],
+        )
+    elif args.data == "ett":
+        from dataset.ett import get_dataloader
+        train_loader, valid_loader, test_loader, scaler = get_dataloader(
+            seed=config["data"]["seed"],
+            nfold=args.nfold,
+            batch_size=config["data"]["batch_size"],
+            missing_ratio=args.missrate, # config["data"]["test_missing_ratio"],
+            missing_pattern=args.misspattern, # config["data"]["missing_pattern"],
+            num_steps=config["data"]["num_steps"],
+        )
+    elif args.data == "weather":
+        from dataset.weather import get_dataloader
+        train_loader, valid_loader, test_loader, scaler = get_dataloader(
+            seed=config["data"]["seed"],
+            nfold=args.nfold,
+            batch_size=config["data"]["batch_size"],
+            missing_ratio=args.missrate, # config["data"]["test_missing_ratio"],
+            missing_pattern=args.misspattern, # config["data"]["missing_pattern"],
+            num_steps=config["data"]["num_steps"],
+        )
+    elif args.data == "metr_la":
+        from dataset.metrla import get_dataloader
+        train_loader, valid_loader, test_loader, scaler = get_dataloader(
+            seed=config["data"]["seed"],
+            nfold=args.nfold,
+            batch_size=config["data"]["batch_size"],
+            missing_ratio=args.missrate, # config["data"]["test_missing_ratio"],
+            missing_pattern=args.misspattern, # config["data"]["missing_pattern"],
+            num_steps=config["data"]["num_steps"],
+        )
+    elif args.data == "yeast":
+        from dataset.yeast import get_dataloader
+        train_loader, valid_loader, test_loader, scaler = get_dataloader(
+            seed=config["data"]["seed"],
+            nfold=args.nfold,
+            batch_size=config["data"]["batch_size"],
+            missing_ratio=args.missrate, # config["data"]["test_missing_ratio"],
+            missing_pattern=args.misspattern, # config["data"]["missing_pattern"],
+            num_steps=config["data"]["num_steps"],
+        )
+    return train_loader, valid_loader, test_loader, scaler
+
+def get_model_optimizer(args, config):
+    if args.model == "csdi":
+        from model.csdi.model import CSDI
+        config_diff = config["diffusion"]
+        model = CSDI(
+                data_name=args.data,
+                num_features=config["data"]["num_features"],
+                num_layers=config_diff["layers"],
+                num_heads=config_diff["nheads"],
+                num_channels=config_diff["channels"],
+                num_diffusion_steps=config_diff["num_steps"],
+                dim_time_embedding=config["model"]["timeemb"],
+                dim_feature_embedding=config["model"]["featureemb"],
+                dim_diffusion_embedding=config_diff["diffusion_embedding_dim"],
+                is_unconditional=config["model"]["is_unconditional"],
+                schedule=config_diff["schedule"],
+                beta_start=config_diff["beta_start"],
+                beta_end=config_diff["beta_end"],
+                target_strategy=config["model"]["target_strategy"],
+                device=args.device,
+            )
+        optimizer = Adam(model.parameters(), lr=config["train"]["lr"], weight_decay=1e-6)
+    elif args.model == "csdi_ori":
+        from model.csdi_ori.model import CSDI_base
+        config_diff = config["diffusion"]
+        model = CSDI_base(
+                data_name=args.data,
+                num_features=config["data"]["num_features"],
+                num_layers=config_diff["layers"],
+                num_heads=config_diff["nheads"],
+                num_channels=config_diff["channels"],
+                num_diffusion_steps=config_diff["num_steps"],
+                dim_time_embedding=config["model"]["timeemb"],
+                dim_feature_embedding=config["model"]["featureemb"],
+                dim_diffusion_embedding=config_diff["diffusion_embedding_dim"],
+                is_unconditional=config["model"]["is_unconditional"],
+                schedule=config_diff["schedule"],
+                beta_start=config_diff["beta_start"],
+                beta_end=config_diff["beta_end"],
+                target_strategy=config["model"]["target_strategy"],
+                device=args.device,
+            )
+        optimizer = Adam(model.parameters(), lr=config["train"]["lr"], weight_decay=1e-6)
+    elif args.model == "fadti":
+        from model.fadti.model import FADTI
+        config_diff = config["diffusion"]
+        model = FADTI(
+            data_name=args.data,
+            num_features=config["data"]["num_features"],
+            num_steps=config["data"]["num_steps"],
+            num_layers=config_diff["layers"],
+            num_heads=config_diff["nheads"],
+            num_channels=config_diff["channels"],
+            num_diffusion_steps=config_diff["num_steps"],
+            dim_time_embedding=config["model"]["timeemb"],
+            dim_feature_embedding=config["model"]["featureemb"],
+            dim_diffusion_embedding=config_diff["diffusion_embedding_dim"],
+            is_unconditional=config["model"]["is_unconditional"],
+            schedule=config_diff["schedule"],
+            beta_start=config_diff["beta_start"],
+            beta_end=config_diff["beta_end"],
+            target_strategy=config["model"]["target_strategy"],
+            method="dft", # "fsst", "frsst"
+            type_layer="attn", # "attn" "conv" "atten+conv"
+            device=args.device,
+        )
+        optimizer = AdamW(model.parameters(), lr=config["train"]["lr"], weight_decay=1e-6)
+    elif args.model == "saits":
+        from model.saits.model import SAITS
+        config_diff = config["diffusion"]
+        model = SAITS(
+                data_name=args.data,
+                n_groups=config["model"]['n_groups'],
+                n_group_inner_layers=config["model"]['n_group_inner_layers'],
+                dim_time=config["data"]["num_steps"],#config["model"]['n_groups'],
+                dim_feature=config["data"]["num_features"],#=config["model"]['n_groups'],
+                dim_model=config["model"]['dim_model'],
+                dim_hidden=config["model"]['dim_hidden'],
+                num_heads=config["model"]['num_heads'], # num_heads
+                dim_k=config["model"]['dim_k'],
+                dim_v=config["model"]['dim_v'],
+                dropout=config["model"]['dropout'],
+                reconstruction_loss_weight=config["model"]['reconstruction_loss_weight'],
+                imputation_loss_weight=config["model"]['imputation_loss_weight'],
+                diagonal_attention_mask=config["model"]['diagonal_attention_mask'],
+                param_sharing_strategy=config["model"]['param_sharing_strategy'],
+                input_with_mask=config["model"]['input_with_mask'],
+                MIT=config["model"]['MIT'],
+                device=args.device,
+            )
+        optimizer = Adam(model.parameters(), lr=config["train"]["lr"], weight_decay=1e-6)
+    elif args.model == "mean":
+        from model.mean.model import Mean
+        config_diff = config["diffusion"]
+        model = Mean(
+            data_name=args.data,
+            device=args.device,
+        )
+        optimizer = None
+    elif args.model == "median":
+        from model.median.model import Median
+        config_diff = config["diffusion"]
+        model = Median(
+            data_name=args.data,
+            device=args.device,
+        )
+        optimizer = None
+    elif args.model == "knn":
+        from model.knn.model import KNN
+        config_diff = config["diffusion"]
+        model = KNN(
+            data_name=args.data,
+            device=args.device,
+        )
+        optimizer = None
+    elif args.model == "brits":
+        from model.brits.model import BRITS
+        # config_diff = config["diffusion"]
+        model = BRITS(
+            data_name=args.data,
+            num_features=config["data"]["num_features"],
+            num_channels=config["model"]["channels"],
+            num_steps=config["data"]["num_steps"],
+            device=args.device,
+        )
+        optimizer = Adam(model.parameters(), lr=config["train"]["lr"], weight_decay=1e-6)
+    elif args.model == "timesnet":
+        from model.timesnet.model import TimesNet
+        # config_diff = config["diffusion"]
+        model = TimesNet(
+            data_name=args.data,
+            num_features=config["data"]["num_features"],
+            num_steps=config["data"]["num_steps"],
+            num_layers=config["model"]["layers"],
+            top_k=config["model"]["top_k"],
+            d_model=config["model"]["dim_model"],
+            d_ffn=config["model"]["dim_ffn"],
+            num_kernels=config["model"]["num_kernels"],
+            dropout=config["model"]['dropout'],
+            device=args.device,
+        )
+        optimizer = Adam(model.parameters(), lr=config["train"]["lr"], weight_decay=1e-6)
+    elif args.model == "mtsci":
+        from model.mtsci.model import MTSCI
+        config_diff = config["diffusion"]
+        model = MTSCI(
+            data_name=args.data,
+            target_dim=config["data"]["num_features"],
+            seqlen=config["data"]["num_steps"], # config_diff["seqlen"],
+            layers=config_diff["layers"],
+            nheads=config_diff["nheads"],
+            channels=config_diff["channels"],
+            num_steps=config_diff["num_steps"],
+            timeemb=config["model"]["timeemb"],
+            featureemb=config["model"]["featureemb"],
+            diffusion_embedding_dim=config_diff["diffusion_embedding_dim"],
+            schedule=config_diff["schedule"],
+            beta_start=config_diff["beta_start"],
+            beta_end=config_diff["beta_end"],
+            alpha=config["train"]["alpha"],
+            beta=config["train"]["beta"],
+            # config, 
+            device=args.device, 
+            # seq_len=config["data"]["num_steps"]
+        )
+        optimizer = Adam(model.parameters(), lr=config["train"]["lr"], weight_decay=1e-6)
+    elif args.model == "timemixer":
+        from model.timemixer.model import TimeMixer
+        # config_diff = config["diffusion"]
+        model = TimeMixer(
+            data_name=args.data,
+            num_features=config["data"]["num_features"],
+            num_steps=config["data"]["num_steps"],
+            num_layers=config["model"]["layers"],
+            top_k=config["model"]["top_k"],
+            d_model=config["model"]["dim_model"],
+            d_ffn=config["model"]["dim_ffn"],
+            channel_independence=config["model"]["channel_independence"],
+            decomp_method=config["model"]["decomp_method"],
+            moving_avg=config["model"]["moving_avg"],
+            downsampling_layers=config["model"]["down_sampling_layers"],
+            downsampling_window=config["model"]["down_sampling_window"],
+            dropout=config["model"]['dropout'],
+            device=args.device,
+        )
+        optimizer = Adam(model.parameters(), lr=config["train"]["lr"], weight_decay=1e-6)
+    elif args.model == "timemixerpp":
+        from model.timemixerpp.model import TimeMixerPP
+        # config_diff = config["diffusion"]
+        model = TimeMixerPP(
+            data_name=args.data,
+            num_features=config["data"]["num_features"],
+            num_steps=config["data"]["num_steps"],
+            num_layers=config["model"]["layers"],
+            top_k=config["model"]["top_k"],
+            d_model=config["model"]["dim_model"],
+            d_ffn=config["model"]["dim_ffn"],
+            channel_independence=config["model"]["channel_independence"],
+            decomp_method=config["model"]["decomp_method"],
+            moving_avg=config["model"]["moving_avg"],
+            downsampling_layers=config["model"]["down_sampling_layers"],
+            downsampling_window=config["model"]["down_sampling_window"],
+            dropout=config["model"]['dropout'],
+            task_name=config["model"]['task_name'],
+            n_heads=config["model"]['n_heads'],
+            n_kernels=config["model"]['n_kernels'],
+            channel_mixing=config["model"]['channel_mixing'],
+            device=args.device,
+        )
+        optimizer = Adam(model.parameters(), lr=config["train"]["lr"], weight_decay=1e-6)
+    return model, optimizer
--- utils/utils.py
+++ utils/utils.py
@@ -1,4 +1,5 @@
 import numpy as np
+import torch
 
 def create_gt_mask(single_mask, missing_ratio=0.1, rng=None):
     # single_mask 的 shape 为 (H, W, ...) 表示单个数据的掩码
@@ -73,3 +74,102 @@ def data_normalize(observed_values, observed_masks, num_features):
     observed_values = ((observed_values - mean) / std * observed_masks)
 
     return observed_values 
+
+class StandardScaler:
+    def fit(self, x: np.ndarray):
+        # x: shape = (N, C, T)
+        N, C, T = x.shape
+        x_flat = x.transpose(0, 2, 1).reshape(-1, C)  # (N*T, C)
+        self.mean = np.mean(x_flat, axis=0)  # shape: (C,)
+        self.std = np.std(x_flat, axis=0) + 1e-6      # shape: (C,)
+        return self
+
+    def transform(self, x: np.ndarray):
+        return (x - self.mean[None, :, None]) / self.std[None, :, None]
+
+    def inverse_transform(self, x: np.ndarray):
+        return x * self.std[None, :, None] + self.mean[None, :, None]
+
+    def fit_torch(self, x: torch.Tensor):
+        # x: (B, C, T)
+        x_reshaped = x.permute(0, 2, 1).reshape(-1, x.shape[1])  # (B*T, C)
+        self.mean = x_reshaped.mean(dim=0)  # (C,)
+        self.std = x_reshaped.std(dim=0) + 1e-6
+        return self
+
+    def transform_torch(self, x: torch.Tensor):
+        mean = torch.tensor(self.mean, dtype=torch.float32).to(x.device)
+        std = torch.tensor(self.std, dtype=torch.float32).to(x.device)
+        return (x - mean[None, :, None]) / std[None, :, None]
+
+    def inverse_transform_torch(self, x: torch.Tensor, device):
+        # print(self.mean, self.std)
+        mean = torch.tensor(self.mean, dtype=torch.float32).to(device)
+        std = torch.tensor(self.std, dtype=torch.float32).to(device)
+        # print(mean, std)
+        return x * std[None, :, None] + mean[None, :, None]
+
+
+import numpy as np
+import torch
+
+class MaskedStandardScaler:
+    def __init__(self):
+        self.mean = None
+        self.std = None
+        self.fitted = False
+
+    def fit(self, values, masks):
+        """
+        values: shape (N, C, T)  (NumPy or torch.Tensor)
+        masks:  same shape, 1 = observed, 0 = missing
+        """
+        if isinstance(values, torch.Tensor):
+            values = values.detach().cpu().numpy()
+        if isinstance(masks, torch.Tensor):
+            masks = masks.detach().cpu().numpy()
+
+        N, C, T = values.shape
+        tmp_values = values.reshape(-1, C)     # (N*T, C)
+        tmp_masks = masks.reshape(-1, C)       # (N*T, C)
+
+        self.mean = np.zeros(C)
+        self.std = np.zeros(C)
+
+        for k in range(C):
+            observed = tmp_values[:, k][tmp_masks[:, k] == 1]
+            self.mean[k] = observed.mean()
+            self.std[k] = observed.std() + 1e-6
+
+        self.fitted = True
+        return self
+
+    def transform(self, values, masks):
+        assert self.fitted, "Must fit before transform."
+        if isinstance(values, torch.Tensor):
+            values_np = values.detach().cpu().numpy()
+            masks_np = masks.detach().cpu().numpy()
+            scaled = ((values_np - self.mean[None, :, None]) / self.std[None, :, None]) * masks_np
+            return torch.tensor(scaled, dtype=values.dtype).to(values.device)
+        else:
+            return ((values - self.mean[None, :, None]) / self.std[None, :, None]) * masks
+
+    def inverse_transform(self, values, masks):
+        assert self.fitted, "Must fit before inverse_transform."
+        if isinstance(values, torch.Tensor):
+            values_np = values.detach().cpu().numpy()
+            masks_np = masks.detach().cpu().numpy()
+            restored = (values_np * self.std[None, :, None] + self.mean[None, :, None]) * masks_np
+            return torch.tensor(restored, dtype=values.dtype).to(values.device)
+        else:
+            return (values * self.std[None, :, None] + self.mean[None, :, None]) * masks
+
+    def save(self, path):
+        assert self.fitted, "Scaler not fitted yet."
+        np.savez(path, mean=self.mean, std=self.std)
+
+    def load(self, path):
+        stats = np.load(path)
+        self.mean = stats['mean']
+        self.std = stats['std']
+        self.fitted = True
